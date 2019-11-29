c4651d4abb58915a31ca0244e422c02943ceb0ff
Bert Maurau <hello@bertmaurau.be>
2019-11-29 11:30:36 +0100

    tweaks

--- core/ValidatedRequest.php
+++ core/ValidatedRequest.php
@@ -13,12 +13,14 @@ use Psr\Http\Message\ServerRequestInterface;
 class ValidatedRequest
 {
 
+    // variable types
     const TYPE_STRING = 'string';
     const TYPE_INTEGER = 'integer';
     const TYPE_FLOAT = 'float';
     const TYPE_URL = 'url';
     const TYPE_BOOLEAN = 'boolean';
     const TYPE_EMAIL = 'email';
+    // variable input methods
     const METHOD_POST = 'POST';
     const METHOD_GET = 'GET';
     const METHOD_ARG = 'ARG';
@@ -70,25 +72,25 @@ class ValidatedRequest
                     if (!isset($_GET[$validationField['field']]) && $validationField['required']) {
                         $instance -> isValid = false;
                         $instance -> output = Output::MissingParameter($response, $validationField['field']);
-                        return $instance;
+                    } else {
+                        $value = filter_input(INPUT_GET, $validationField['field'], self::getFilterSanitizationType($validationField['type']));
                     }
-                    $value = filter_input(INPUT_GET, $validationField['field'], self::getFilterSanitizationType($validationField['type']));
                     break;
                 case self::METHOD_POST:
                     if (!isset($payload[$validationField['field']]) && $validationField['required']) {
                         $instance -> isValid = false;
                         $instance -> output = Output::MissingParameter($response, $validationField['field']);
-                        return $instance;
+                    } else {
+                        $value = filter_var($payload[$validationField['field']], self::getFilterSanitizationType($validationField['type']));
                     }
-                    $value = filter_var($payload[$validationField['field']], self::getFilterSanitizationType($validationField['type']));
                     break;
                 case self::METHOD_ARG:
                     if (!isset($input[$validationField['field']]) && $validationField['required']) {
                         $instance -> isValid = false;
                         $instance -> output = Output::MissingParameter($response, $validationField['field']);
-                        return $instance;
+                    } else {
+                        $value = filter_var($input[$validationField['field']], self::getFilterSanitizationType($validationField['type']));
                     }
-                    $value = filter_var($input[$validationField['field']], self::getFilterSanitizationType($validationField['type']));
                     break;
                 default:
                     break;
@@ -97,7 +99,6 @@ class ValidatedRequest
             if (!$value) {
                 $instance -> isValid = false;
                 $instance -> output = Output::InvalidParameter($response, $validationField['field']);
-                return $instance;
             } else {
                 $instance -> filteredInput[$validationField['field']] = $value;
             }
@@ -114,11 +115,11 @@ class ValidatedRequest
      * @param int $type
      * @param string $variable_name
      * @param int $filter
-     * @param type $options
+     * @param int $options
      *
      * @return mixed
      */
-    public static function filterInput(int $type, string $variable_name, int $filter = FILTER_DEFAULT, $options = NULL)
+    public static function filterInput(int $type, string $variable_name, int $filter = FILTER_DEFAULT, int $options = NULL)
     {
         $checkTypes = [
             INPUT_GET,
@@ -132,15 +133,16 @@ class ValidatedRequest
             $options = FILTER_NULL_ON_FAILURE;
         }
 
+        $filteredVal = null;
         if (in_array($type, $checkTypes) || filter_has_var($type, $variable_name)) {
-            return filter_input($type, $variable_name, $filter, $options);
+            $filteredVal = filter_input($type, $variable_name, $filter, $options);
         } else if ($type == INPUT_SERVER && isset($_SERVER[$variable_name])) {
-            return filter_var($_SERVER[$variable_name], $filter, $options);
+            $filteredVal = filter_var($_SERVER[$variable_name], $filter, $options);
         } else if ($type == INPUT_ENV && isset($_ENV[$variable_name])) {
-            return filter_var($_ENV[$variable_name], $filter, $options);
-        } else {
-            return NULL;
+            $filteredVal = filter_var($_ENV[$variable_name], $filter, $options);
         }
+
+        return $filteredVal;
     }
 
     /**
@@ -174,30 +176,34 @@ class ValidatedRequest
     }
 
     /**
-     * Get the Sanitization Type for given variable type
+     * Get the Sanitizing Type for given variable type
      *
      * @param string $type The type of variable
      *
-     * @return int The number of the sanitization type
+     * @return int The number of the sanitizing type
      */
     private static function getFilterSanitizationType(string $type): int
     {
+
+        $filterVal = 0;
         switch ($type) {
             case self::TYPE_STRING:
-                return FILTER_SANITIZE_STRING;
+                $filterVal = FILTER_SANITIZE_STRING;
             case self::TYPE_INTEGER:
-                return FILTER_SANITIZE_NUMBER_INT;
+                $filterVal = FILTER_SANITIZE_NUMBER_INT;
             case self::TYPE_FLOAT:
-                return FILTER_SANITIZE_NUMBER_FLOAT;
+                $filterVal = FILTER_SANITIZE_NUMBER_FLOAT;
             case self::TYPE_EMAIL:
-                return FILTER_SANITIZE_EMAIL;
+                $filterVal = FILTER_SANITIZE_EMAIL;
             case self::TYPE_BOOLEAN:
-                return FILTER_VALIDATE_BOOLEAN;
+                $filterVal = FILTER_VALIDATE_BOOLEAN;
             case self::TYPE_URL:
-                return FILTER_VALIDATE_URL;
+                $filterVal = FILTER_VALIDATE_URL;
             default:
-                return 0;
+                $filterVal = 0;
         }
+
+        return $filterVal;
     }
 
 }
